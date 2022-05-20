d491c2245fe104afe99aec319b7a396f9e0e8d2f
BorisPavlov <pbd_b@mail.ru>
2022-05-20 15:34:17 +0300

    create project and parse json

--- NewsFeed/Service/NetworkManager.swift
+++ NewsFeed/Service/NetworkManager.swift
@@ -72,39 +72,3 @@ class NetworkManager {
     }
     
 }
-
-/*
- func postRequest(with data: [String: Any], to url: String, completion: @escaping(Result<Any, NetworkError>) -> Void) {
-     guard let url = URL(string: url) else {
-         completion(.failure(.invalidURL))
-         return
-     }
-     
-     guard let courseData = try? JSONSerialization.data(withJSONObject: data) else {
-         completion(.failure(.noData))
-         return
-     }
-     
-     var request = URLRequest(url: url)
-     request.httpMethod = "POST"
-     request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-     request.httpBody = courseData
-     
-     URLSession.shared.dataTask(with: request) { data, response, error in
-         guard let data = data, let response = response else {
-             completion(.failure(.noData))
-             print(error?.localizedDescription ?? "No error description")
-             return
-         }
-         
-         print(response)
-         
-         do {
-             let course = try JSONSerialization.jsonObject(with: data)
-             completion(.success(course))
-         } catch {
-             completion(.failure(.decodingError))
-         }
-     }.resume()
- }
- */
